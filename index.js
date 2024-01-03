a7e628eec2235efe5e60f9bc1b330d08f47ab9da
Kenny Elias <kennyegun240@gmail.com>
2024-01-03 17:12:08 +0100

    modify

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const bodyParser = require("body-parser");
+const paymentCheckout = require("./routes/payment");
+const dotenv = require("dotenv");
+dotenv.config();
 app.use(bodyParser.json());
 // app.use(express.json())
 app.use(
@@ -10,40 +13,12 @@ app.use(
   })
 );
 
-const client_sec =
-  "sk_test_51N1y0eFGKykGLNp4cycVEz1dGfnembei9I9xymJbk9YixTnLy5Igr6zqEEO5CveQvpMlRs7CV7ofV4NDLfFdjnnc00AlX8wEyB";
+const client_sec = process.env.STRIPE_KEY;
 
 const stripe = require("stripe")(client_sec);
 
-app.get("/payment-config", (req, res) => {
-  res.send();
-});
-app.post("/create-checkout-session", async (req, res) => {
-  const { products } = req.body;
-  const line_items = products.map((product) => ({
-    price_data: {
-      currency: "usd",
-      product_data: {
-        name: product.product_name,
-        images: [product.product_image],
-      },
-      unit_amount: Math.round(product.price * 100),
-    },
-    quantity: product.quantity,
-  }));
-
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    line_items: line_items,
-    mode: "payment",
-    success_url: "http://localhost:3000/payment/success",
-    cancel_url: "http://localhost:3000/payment/failure",
-  });
-  console.log(session.id);
-
-  res.json({ id: session.id });
-});
+app.use("/api/payment", paymentCheckout);
 
 app.listen(4000, () => {
-  console.log("running");
+  console.log("server is running");
 });
---
+++ routes/payment.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+
+router.post("/create-checkout-session", async (req, res) => {
+  const { products } = req.body;
+  const line_items = products.map((product) => ({
+    price_data: {
+      currency: "usd",
+      product_data: {
+        name: product.product_name,
+        images: [product.product_image],
+      },
+      unit_amount: Math.round(product.price * 100),
+    },
+    quantity: product.quantity,
+  }));
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: line_items,
+    mode: "payment",
+    success_url: "http://localhost:3000/payment/success",
+    cancel_url: "http://localhost:3000/payment/failure",
+  });
+
+  res.json({ id: session.id });
+});
+
+module.exports = router;
