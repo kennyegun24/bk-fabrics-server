23c51752f8adb0beedb514317c24c102952f9652
Kenny Elias <kennyegun240@gmail.com>
2024-01-03 16:59:31 +0100

    Modify

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-
+// app.use(express.json())
 app.use(
   cors({
     origin: "http://localhost:3000",
