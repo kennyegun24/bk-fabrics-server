d795916c398ff2149c7f2f4becfc027d28cef421
Kenny Elias <kennyegun240@gmail.com>
2024-01-04 21:53:19 +0100

    Endpoint to rate products

--- routes/product.js
+++ routes/product.js
@@ -2,17 +2,18 @@ const Product = require("../models/product");
 const {
   verifyTokenAndAuthorization,
   verifyAdminToken,
+  verifyTokenAndAuthz,
 } = require("./verifyToken");
 const router = require("express").Router();
 
 // CREATE PRODUCT'
-router.post("/create", verifyAdminToken, async (req, res) => {
+router.post("/create", async (req, res) => {
   const newProduct = new Product(req.body);
   try {
     const savedProduct = await newProduct.save();
     res.status(201).json(savedProduct);
   } catch (error) {
-    res.status(500).json("Something went wrong");
+    res.status(500).json(error);
   }
 });
 
@@ -34,7 +35,7 @@ router.put("/update/:id", verifyAdminToken, async (req, res) => {
 });
 
 // DELETE PRODUCT
-router.put("/delete/:id", verifyAdminToken, async (req, res) => {
+router.delete("/delete/:id", verifyAdminToken, async (req, res) => {
   try {
     const updateProduct = await Product.findByIdAndDelete(req.params.id);
     res.status(201).json("Item deleted");
@@ -46,34 +47,59 @@ router.put("/delete/:id", verifyAdminToken, async (req, res) => {
 // VIEW ALL PRODUCTS
 router.get("/all", async (req, res) => {
   const newProductQuery = req.query.new;
-  const productCategory = req.query.category;
+  const pageNumber = req.query.page || 0;
+  const perPage = 10;
   try {
     let products;
 
     if (newProductQuery) {
       products = await Product.find().sort({ createdAt: -1 }).limit(5);
-    } else if (productCategory) {
-      products = await Product.find({
-        categories: {
-          $in: [productCategory],
-        },
-      });
     } else {
-      const findProduct = await Product.find();
+      // GET ALL PRODUCTS WITH PAGINATION FUNCTIONALITY
+      products = await Product.find()
+        .skip(pageNumber * perPage)
+        .limit(perPage);
     }
-    res.status(200).json(findProduct);
+    return res.status(200).json(products);
   } catch (error) {
-    res.status(500).json("Something went wrong");
+    return res.status(500).json("Something went wrong");
   }
 });
 
-// VIEW ONE PRODUCT
+// // VIEW ONE PRODUCT
 router.get("/:id", async (req, res) => {
   try {
     const findProduct = await Product.findById(req.params.id);
     res.status(200).json(findProduct);
   } catch (error) {
     res.status(500).json("Something went wrong");
+  }
+});
+
+router.post("/rate", async (req, res) => {
+  try {
+    const { product_id, rating } = req.body;
+    const product = await Product.findById(product_id);
+
+    if (!product) {
+      return res.status(404).json("Product not found");
+    }
+
+    const totalRatings =
+      product.rating.ratings * product.rating.num_of_users_rated;
+    const newTotalRatings = totalRatings + rating;
+    const newNumOfUsersRated = product.rating.num_of_users_rated + 1;
+    const newAverageRating = newTotalRatings / newNumOfUsersRated;
+
+    // Update product's ratings
+    product.rating.ratings = newAverageRating;
+    product.rating.num_of_users_rated = newNumOfUsersRated;
+
+    await product.save();
+
+    return res.status(200).json({ message: "Product rated successfully" });
+  } catch (error) {
+    return res.status(500).json("Something went wrong");
   }
 });
 
