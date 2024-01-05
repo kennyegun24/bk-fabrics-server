389f0362ac09f57c3d9e4465022998979803a099
Kenny Elias <kennyegun240@gmail.com>
2024-01-05 22:52:52 +0100

    Handle customers orders

--- routes/order.js
+++ routes/order.js
@@ -1,12 +1,38 @@
 const router = require("express").Router();
 const Order = require("../models/order");
+const Product = require("../models/product");
 const { verifyTokenAndAuthz, verifyAdminToken } = require("./verifyToken");
 
-router.post("/", verifyTokenAndAuthz, async (req, res) => {
+router.post("/", async (req, res) => {
   try {
-    const newOrder = new Order(req.body);
+    let prods = [];
+    let amount = 0;
+    const { products } = req.body;
+    const line_items = await Promise.all(
+      products.map(async (product) => {
+        const prod = await Product.findById(product.id);
+        const stock = prod.in_stock - product.quantity;
+        const sold = prod.sold + product.quantity;
+        prod.in_stock = stock;
+        prod.sold = sold;
+        prod.save();
+
+        if (!prod) {
+          return null;
+        }
+        amount += prod.price;
+        prods.push(prod);
+      })
+    );
+    const newOrder = new Order({
+      userId: req.body.userId,
+      address: req.body.address,
+      products: prods,
+      amount,
+    });
     await newOrder.save();
     return res.status(201).json("Order successfully created");
+    prods = [];
   } catch (error) {
     return res.status(500).json("Something went wrong");
   }
