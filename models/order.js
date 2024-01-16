62a9a6eb0001eb6e1843d7a1a3176abb0b7952d3
Kenny Elias <kennyegun240@gmail.com>
2024-01-16 13:30:16 +0100

    Modify model structures

--- models/order.js
+++ models/order.js
@@ -5,6 +5,13 @@ const orderSchema = new mongoose.Schema(
     userId: { type: String, required: true },
     amount: { type: Number, required: true },
     address: { type: String, required: true },
+    postalCode: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    country: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
     status: { type: String, default: "Pending" },
     products: [
       {
--- models/user.js
+++ models/user.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    user_name: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    address: { type: Number },
     is_admin: { type: Boolean, required: true, default: false },
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
   },
   { timestamps: true }
 );
